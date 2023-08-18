c0bbad5aa746f861d03482fb104280cd3e6be96a
bobJiang <374123140@qq.com>
2023-08-18 09:53:58 +0800

    init project

---
+++ dao/redis/redis.go
@@ -0,0 +1,35 @@
+package redis
+
+import (
+	"bluebell/settings"
+	"fmt"
+	"github.com/go-redis/redis"
+	"go.uber.org/zap"
+)
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// Init 初始化连接
+func Init() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%d",
+			settings.Conf.RedisConfig.Host,
+			settings.Conf.RedisConfig.Port),
+		Password: settings.Conf.RedisConfig.Password,
+		// use default DB
+		DB:       settings.Conf.RedisConfig.DB,
+		PoolSize: settings.Conf.RedisConfig.PoolSize,
+	})
+
+	_, err = rdb.Ping().Result()
+	if err != nil {
+		zap.L().Error("redis connect failed", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+func Close() {
+	_ = rdb.Close()
+}
---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"bluebell/logger"
+	"github.com/gin-gonic/gin"
+)
+
+func Setup() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "ok")
+	})
+	return r
+}
