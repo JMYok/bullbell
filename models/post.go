985f7852036e4dedb9363fa55bdc79921795a23e
BobJiang <374123140@qq.com>
2023-09-03 11:20:11 +0800

    fix post结构体内存对齐

--- models/post.go
+++ models/post.go
@@ -1,12 +1,14 @@
 package models
 
+// 内存对齐
+
 type Post struct {
 	Id          uint64 `json:"id" db:"post_id"`
-	Title       string `json:"title" db:"title"`
-	Content     string `json:"content" db:"content"`
 	AuthorId    int64  `json:"author_id" db:"author_id"`
 	CommunityId int64  `json:"community_id" db:"community_id"`
 	Status      int8   `json:"status" db:"status"`
+	Title       string `json:"title" db:"title"`
+	Content     string `json:"content" db:"content"`
 	CreateTime  string `json:"create_time" db:"create_time"`
 	UpdateTime  string `json:"update_time" db:"update_time"`
 }
